9f7bee5281571179d71a3500c539fc66e770124c
Kevin Pouget <kpouget@redhat.com>
2023-10-20 20:55:11 +0200

    ble_watch: new bluetooth watcher

---
+++ ble_watch/ble_watch.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+# inspired by https://github.com/madkaye/ble-ls
+
+import sys
+import datetime
+import json
+import logging
+logging.getLogger().setLevel(logging.INFO)
+import argparse
+
+from bluepy import btle
+from bluepy.btle import Scanner, Peripheral, Characteristic, ScanEntry, UUID
+
+import trame
+
+
+class BLELS:
+
+    SCAN_TIMEOUT = 10
+    scanner = None
+    publicdevices = []
+
+    def scan(self, process_data, duration=SCAN_TIMEOUT):
+        print("scan: starting scan for {}s".format(duration))
+        self.scanner = Scanner()
+        devices = self.scanner.scan(duration)
+        foundDevices = 0
+        for dev in devices:
+            devname = dev.getValueText(btle.ScanEntry.COMPLETE_LOCAL_NAME)
+            if devname is None:
+                devname = dev.getValueText(btle.ScanEntry.SHORT_LOCAL_NAME)
+
+            if process_data(devname, dev):
+                return True
+
+    def connectandread(self, addr):
+        try:
+
+            peri = Peripheral()
+            peri.connect(addr)
+
+            print("Listing services...")
+            services = peri.getServices()
+            for serv in services:
+                print("   -- SERVICE: {} [{}]".format(serv.uuid, UUID(serv.uuid).getCommonName()))
+                characteristics = serv.getCharacteristics()
+                for chara in characteristics:
+                    print("   --   --> CHAR: {}, Handle: {} (0x{:04x}) - {} - [{}]".format(chara.uuid,
+                                                                                    chara.getHandle(),
+                                                                                    chara.getHandle(),
+                                                                                    chara.propertiesToString(),
+                                                                                    UUID(chara.uuid).getCommonName()))
+            print("Listing descriptors...")
+            descriptors = peri.getDescriptors()
+            for desc in descriptors:
+                print("   --  DESCRIPTORS: {}, [{}], Handle: {} (0x{:04x})".format(desc.uuid,
+                                                                                    UUID(desc.uuid).getCommonName(),
+                                                                                    desc.handle, desc.handle))
+
+            print("Reading characteristics...")
+            chars = peri.getCharacteristics()
+            for c in chars:
+                print("  -- READ: {} [{}] (0x{:04x}), {}, Value: {}".format(c.uuid, UUID(c.uuid).getCommonName(),
+                                                                c.getHandle(), c.descs, c.read() if c.supportsRead() else ""))
+
+
+        except Exception as e:
+            print("connectandread: Error,", e)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Xiaomi ATC BLE watcher")
+    parser.add_argument("--target", help="Name and MAC of the device")
+    parser.add_argument("--mine", help="Show my devices", action="store_true")
+    parser.add_argument("--duration", help="Duration of the scan", default=10)
+    parser.add_argument("--tries", help="Number of tries to perform before failing", default=3)
+
+    try:
+        args, unknown = parser.parse_known_args()
+        logging.info(args)
+
+    except argparse.ArgumentError as exc:
+        print("%s" % exc, file=sys.stderr)
+        sys.exit(ERROR_ARG)
+
+    if args.mine or not args.target:
+        print(sys.argv[0], "--target bleu_A4:C1:38:45:AF:D5")
+        sys.exit(0)
+
+    logging.info(f"Target: {args.target}")
+    location, mac = args.target.split("_")
+
+    print("--- BLE LS Script ---")
+    print(f"mac:      {mac}")
+    print(f"location: {location}")
+    print("--------------------")
+
+    def process_data(devname, dev):
+        if dev.addr.lower() != mac.lower():
+            return
+
+        bin_data = (b"\x12\x16" + dev.scanData[22])
+
+        data = trame.decode(location, bin_data)
+
+        if not data: return
+
+        data["date"] = str(datetime.datetime.now())
+        data["time"] = int(datetime.datetime.utcnow().timestamp())
+        dest = f"/tmp/{location}.json"
+        logging.info(f"Saving {dest} ...")
+        with open(dest, "w") as f:
+            json.dump(data, f, indent=4)
+            print("", file=f)
+
+        return True
+
+    for i in range(args.tries):
+        try:
+            if BLELS().scan(process_data, duration=args.duration):
+                break
+            print("Not found")
+        except bluepy.btle.BTLEDisconnectError as e:
+            print("Device disconnected ...")
+        except Exception as e:
+            print(f"Exception {e.__class__.__name__}: {e}")
+
+    else:
+        print("Didn't find any match :/")
+        sys.exit(1)
+
+    print("--------------------")
---
+++ ble_watch/trame.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+import struct
+
+first_trame = b"\xbd\x06\x2f\x01\x73\x1b\x06\x0a\x01\x27\x2c\x00\x00\x98\x01\x00" \
+    b"\x00\xd6\xbe\x89\x8e\x00\x1c\xd5\xaf\x45\x38\xc1\xa4\x02\x01\x06" \
+    b"\x12\x16\x1a\x18\xd5\xaf\x45\x38\xc1\xa4\xfa\x07\x9f\x13\x8d\x0b" \
+    b"\x64\xec\x0e\xfd\x92\xc4"
+
+# https://docs.python.org/3/library/struct.html
+# https://github.com/pvvx/ATC_MiThermometer#atc1441-format
+"""
+uint8_t     size;   // = 18
+uint8_t     uid;    // = 0x16, 16-bit UUID
+uint16_t    UUID;   // = 0x181A, GATT Service 0x181A Environmental Sensing
+
+uint8_t     MAC[6]; // [0] - lo, .. [6] - hi digits
+
+int16_t     temperature;    // x 0.01 degree
+uint16_t    humidity;       // x 0.01 %
+uint16_t    battery_mv;     // mV
+uint8_t     battery_level;  // 0..100 %
+uint8_t     counter;        // measurement count
+uint8_t     flags;  // GPIO_TRG pin (marking "reset" on circuit board) flags:
+                    // bit0: Reed Switch, input
+                    // bit1: GPIO_TRG pin output value (pull Up/Down)
+                    // bit2: Output GPIO_TRG pin is controlled according to the set parameters
+                    // bit3: Temperature trigger event
+                    // bit4: Humidity trigger event
+
+"""
+
+START = 0
+HEADER_LENGTH = 1+1+2
+MAC_LENGTH = 6
+DATA_START = START+HEADER_LENGTH+MAC_LENGTH
+DATA_LENGTH = 2+2+2+1+1+1
+SHOW_HEADERS = False
+
+current_counter = 0
+
+def decode(location, _trame):
+    SIZE_UID_UUID = b"\x12\x16\x1a\x18"
+    starter, found, content = _trame.partition(SIZE_UID_UUID)
+    if not found:
+        print(f"{SIZE_UID_UUID} anchor not found in the data trame:/ {_trame}")
+        return
+
+    trame = SIZE_UID_UUID + content
+    size, uid, uuid = struct.unpack('<BBH', trame[START:START+HEADER_LENGTH])
+    mac = struct.unpack('>HHH', trame[START+HEADER_LENGTH:START+HEADER_LENGTH+MAC_LENGTH])
+    mac_hexa = "".join(map(hex, mac)).replace("0x", "").upper()
+    mac_addr = ":".join(reversed([f"{a}{b}" for a, b in zip(mac_hexa[0::2], mac_hexa[1::2])]))
+
+    keys = ["temperature", "humidity", "batt_mv", "batt_lvl", "counter", "flags"]
+    values = struct.unpack('<hHHBBB', trame[DATA_START:DATA_START+DATA_LENGTH])
+
+    kv = dict(zip(keys, values))
+    kv["temperature"] /= 100
+    kv["humidity"] /= 100
+    g = globals()
+    g.update(kv)
+    kv.pop("flags")
+    global current_counter
+    import datetime
+    print(datetime.datetime.now().time().replace(microsecond=0))
+    if current_counter == counter:
+        return
+
+    if SHOW_HEADERS:
+        print("size\t\t:", size)
+        print("uid\t\t:", hex(uid))
+        print("uuid\t\t:", hex(uuid))
+        print("mac\t\t:", mac_addr)
+        print(f"flags\t\t: {bin(flags)}")
+
+    print(f"### {location}")
+    print(f"temperature\t: {temperature} degree")
+    print(f"humidity\t: {humidity}%")
+    print(f"batt_mv\t\t: {batt_mv}mV")
+    print(f"batt_lvl\t: {batt_lvl}%")
+    print(f"counter\t\t: {counter}")
+    print()
+    current_counter = counter
+
+    return kv
+
+if __name__ == "__main__":
+    #decode(first_trame)
+    from scapy.all import *
+
+    MAC = "A4:C1:38:45:AF:D5"
+
+    mac_filter = "".join(reversed(MAC.lower().split(":")))
+
+    packets = rdpcap(sys.argv[1])
+    for packet in packets:
+        trame = bytes_hex(packet)
+
+        if mac_filter not in str(trame):
+            continue
+
+        decode(bytes(packet))
+
+        print("---")
