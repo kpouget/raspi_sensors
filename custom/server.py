87f49f4ac71ebd79f2cebc9da4325d89d1f5e26d
Kevin Pouget <kpouget@redhat.com>
2023-12-18 20:21:32 +0100

    [custom] update

--- custom/server.py
+++ custom/server.py
@@ -1,18 +1,23 @@
+#! /usr/bin/env python3
+
 import json
 import sys
 import os
+import yaml
 
 import datetime
 import bottle
 import prometheus_client
 import prometheus_client.parser
 
+METRICS_FILE_YAML = "out/metrics.yaml"
 METRICS_FILE = "out/metrics.txt"
-LABELS_COUNT = 3
+CREATE_LABELS_COUNT = 3
 
-existing_metrics = {}
+published_metrics = {}
+top_metrics = {}
+yaml_metrics = {}
 
-last_update = {}
 
 @bottle.route('/static/<filepath:re:.*\.css>')
 def css(filepath):
@@ -20,56 +25,94 @@ def css(filepath):
 
 
 def update_metric(metric_key, update_value, increase=True):
-    metric = existing_metrics[metric_key]
-    current_value = metric._samples()[0].value
+    metric, entry = published_metrics[metric_key]
+    current_value = entry.get("value") or 0
 
     new_value = float(update_value)
     if increase:
         new_value += current_value
 
     metric.set(new_value)
-    last_update[metric_key] = f"le {datetime.datetime.now().strftime('%Y-%m-%d à %H:%M')}", update_value
-
-    return f"{metric._documentation} [{metric_key}] +{update_value} --> {new_value}", True
+    entry["last_update"]["date"] = datetime.datetime.now().strftime('%Y-%m-%d à %H:%M')
+    entry["last_update"]["value"] = float(update_value)
+    entry["value"] = new_value
+    
+    with open(METRICS_FILE_YAML, "w") as f:
+        if not yaml_metrics:
+            raise Exception("yaml is empty ...")
+        print(yaml.dump(yaml_metrics), file=f)
+    
+    return f"{metric._documentation} [{metric_key}] +{update_value}{entry['units']} --> {new_value}{entry['units']}", True
 
 
 def new_metric(forms):
     name = forms["__new__.name"]
-    descr = forms["__new__.description"]
-    if descr == "exit":
+    docu = forms["__new__.documentation"]
+    units = forms["__new__.units"]
+    
+    if docu == "exit":
         sys.stderr.close()
         return "Closing", False
 
-    if descr == "delete":
+    if docu == "delete":
         try:
-            metric = existing_metrics.pop(name)
+            metric, entry = published_metrics.pop(name)
         except KeyError:
-            return f"Métrique {name} n'existe pas :/"
+            return f"Métrique {name} n'existe pas :/", False
         # prometheus_client.registry.REGISTRY.unregister(metric)
 
+        yaml_metrics[metric.name].pop(entry)
+        
         return f"Métrique {name} supprimée", True
 
     labels = {}
-    for label_id in range(LABELS_COUNT):
+    for label_id in range(CREATE_LABELS_COUNT):
         label_name = forms.get(f"__new__.label{label_id}")
         if not label_name: continue
         label_value = forms.get(f"__new__.labelvalue{label_id}", "no value")
         labels[label_name] = label_value
 
-    metric = prometheus_client.Gauge(name, descr, labels.keys())
+    try:
+        metric = top_metrics[name]
+    except KeyError:
+        metric = prometheus_client.Gauge(name, docu, labels.keys())
+        top_metrics[name] = metric
+        
     if labels:
         metric = metric.labels(*labels.values())
+        
     metric.set(0)
 
-    insert_metric(metric)
+    if name not in yaml_metrics:
+        yaml_metrics[name] = dict(
+            documentation=docu,
+            entries=[],
+        )
 
+    entry = dict(
+        labels=labels,
+        last_update=dict(date=None, value=None),
+        value=0,
+        units=units,
+    )
+    
+    yaml_metrics[name]["entries"].append(entry)
+        
+    publish_metric(metric, entry)
+
+    with open(METRICS_FILE_YAML, "w") as f:
+        if not yaml_metrics:
+            raise Exception("yaml is empty ...")
+        print(yaml.dump(yaml_metrics), file=f)
+    
+    
     return f"Métrique {name}{labels} crée :)", True
 
 
-def insert_metric(metric):
+def publish_metric(metric, entry):
     label_id = "-".join([f"{k}_{v}" for k, v in zip(metric._labelnames, metric._labelvalues)])
 
-    existing_metrics[f"{metric._name}--{label_id}"] = metric
+    published_metrics[f"{metric._name}--{label_id}"] = metric, entry
 
 
 @bottle.route('/', method=["POST", "GET"])
@@ -89,32 +132,29 @@ def index():
     else:
         msg = None
 
-    return bottle.template("index", metrics=existing_metrics.items(), msg=msg, LABELS_COUNT=LABELS_COUNT, last_update=last_update)
+    return bottle.template("index", published_metrics=published_metrics.items(), msg=msg, CREATE_LABELS_COUNT=CREATE_LABELS_COUNT)
+
 
 def load_metrics():
-    with open(METRICS_FILE) as f:
-        metrics = f.read()
+    with open(METRICS_FILE_YAML) as f:
+        global yaml_metrics
+        yaml_metrics = yaml.safe_load(f)
 
-    for family in prometheus_client.parser.text_string_to_metric_families(metrics):
-        if family.name.startswith("process_"): continue
-        if family.name.startswith("python_"): continue
-
-        if family.type == "counter":
-            metric_class = prometheus_client.Counter
-        elif family.type == "gauge":
-            metric_class = prometheus_client.Gauge
+    for metric_name, metric_props in yaml_metrics.items():
         metric_labels = set()
 
-        for sample in family.samples:
-            metric_labels |= sample.labels.keys()
+        for entry in metric_props["entries"]:
+            metric_labels |= entry["labels"].keys()
 
-        metric = metric_class(family.name, family.documentation, list(metric_labels))
+        metric = prometheus_client.Gauge(metric_name, metric_props["documentation"],
+                                         list(metric_labels))
+        top_metrics[metric_name] = metric
+        for entry in metric_props["entries"]:
+            if entry["labels"]:
+                full_metric = metric.labels(*entry["labels"].values())
+            full_metric.set(entry.get("value") or 0)
 
-        for sample in family.samples:
-            full_metric = metric.labels(*sample.labels.values()) if sample.labels else metric
-            full_metric.set(sample.value)
-
-            insert_metric(full_metric)
+            publish_metric(full_metric, entry)
 
 
 @bottle.route('/metrics')
@@ -124,8 +164,8 @@ def metrics():
 
 if __name__ == "__main__":
     load_metrics()
-    for m in existing_metrics.values():
-        labels = dict(zip(m._labelnames, m._labelvalues))
-        print(m, m._documentation, labels)
+    for metric, entry in published_metrics.values():
+        labels = dict(zip(metric._labelnames, metric._labelvalues))
+        print(metric, metric._documentation, labels)
 
     bottle.run(host='localhost', port=int(os.environ.get("BOTTLE_PORT", 8080)), reloader=True)
--- custom/views/index.tpl
+++ custom/views/index.tpl
@@ -12,22 +12,29 @@
   <hr/>
   % end
 
-  % for metric_id, metric in metrics:
+  % for metric_id, (metric, entry) in published_metrics:
+    
   <form class="form-inline" action="/" method="POST">
     % import json
     % label_dict = dict(zip(metric._labelnames, metric._labelvalues))
     % label_text = ", ".join(f"{k}: {v}" for k, v in label_dict.items())
     % label_text = "{"+label_text+"}" if label_text else ""
+    % if entry.get("hidden"):
+    % continue
+    % end
+
     <p>
       <input type="hidden" name="what" value="{{ metric_id }}">
-      <input type="number" id="{{ metric_id }}" placeholder="{{ metric_id}} ({{ metric._samples()[0].value }})" name="{{ metric_id }}" required>
+      <input type="float" id="{{ metric_id }}" placeholder="{{ metric._documentation}} {{ metric._samples()[0].value }} {{ entry["units"] }}" name="{{ metric_id }}" required>
       <button type="submit">Ajouter</button>
       <br/>
       <label for="{{ metric_id }}">
-        {{ metric._documentation}} | <code>{{ metric._name }} {{ label_text }}</code></label>
+        {{ metric._documentation}} | <code>{{ metric._name }} {{ label_text }} {{ entry["value"] or 0 }}</code></label>
       <br/>
-      % metric_last_update =  last_update.get(metric_id, ["jamais", 0])
-      <b>Dernière mise à jour:</b> +{{metric_last_update[1]}} {{ metric_last_update[0]}}
+      % metric_last_update_date = entry["last_update"].get("date") or "(jamais)"
+      % metric_last_update_value = entry["last_update"].get("value") or 0
+      % metric_last_update_units = entry["units"]
+      <b>Dernière mise à jour:</b> +{{metric_last_update_value}} {{ metric_last_update_units}} le {{ metric_last_update_date }}
       </br>
       </p>
   </form>
@@ -38,8 +45,10 @@
       Nouvelle métrique:<br/>
       <input type="hidden" name="what" value="__new__">
       <input id="__new__.name" placeholder="Nom de la métrique" name="__new__.name" required>
-      <input id="__new__.description" placeholder="Description de la métrique" name="__new__.description" required> <button type="submit">Créer</button>
-      % for label_id in range(LABELS_COUNT):
+      <input id="__new__.documentation" placeholder="Description ou 'exit' ou 'delete'" name="__new__.documentation"> 
+      <input id="__new__.units" placeholder="Unités" name="__new__.units">
+      <button type="submit">Créer</button>
+      % for label_id in range(CREATE_LABELS_COUNT):
         <br/> Label {{ label_id }}:
         <input id="__new__.labelname{{ label_id }}" placeholder="Label #{{ label_id }}" name="__new__.label{{ label_id }}">
         <input id="__new__.labelvalue{{ label_id }}" placeholder="Valeur du label #{{ label_id }}" name="__new__.labelvalue{{ label_id }}">
